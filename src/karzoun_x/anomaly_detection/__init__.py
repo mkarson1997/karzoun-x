dd6b07c1a9cf4fd2a922a372cbfe922ad3e4b5a5
mahmoud karzoun <135722882+mkarson1997@users.noreply.github.com>
2026-09-09 13:19:59 +0300

    phase2: export adaptive temporal detector

--- src/karzoun_x/anomaly_detection/__init__.py
+++ src/karzoun_x/anomaly_detection/__init__.py
@@ -1,3 +1,8 @@
+from .adaptive_temporal import AdaptiveRobustDetector, apply_persistence_filter
 from .robust_zscore import RobustZScoreDetector
 
-__all__ = ["RobustZScoreDetector"]
+__all__ = [
+    "AdaptiveRobustDetector",
+    "RobustZScoreDetector",
+    "apply_persistence_filter",
+]
