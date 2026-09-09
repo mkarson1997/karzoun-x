6d9dbfe983cc6636e1c22be5c31cff7a8443b67a
mahmoud karzoun <135722882+mkarson1997@users.noreply.github.com>
2026-09-09 14:40:32 +0300

    phase3: export stability-aware detector

--- src/karzoun_x/anomaly_detection/__init__.py
+++ src/karzoun_x/anomaly_detection/__init__.py
@@ -1,8 +1,10 @@
 from .adaptive_temporal import AdaptiveRobustDetector, apply_persistence_filter
 from .robust_zscore import RobustZScoreDetector
+from .stability_aware import StabilityAwareDetector
 
 __all__ = [
     "AdaptiveRobustDetector",
     "RobustZScoreDetector",
+    "StabilityAwareDetector",
     "apply_persistence_filter",
 ]
