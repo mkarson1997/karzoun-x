d6800b6f13950c73c7621ada5a3bb23a97d8ad73
mahmoud karzoun <135722882+mkarson1997@users.noreply.github.com>
2026-09-09 14:49:43 +0300

    phase4: initialize synthetic fault simulator package

---
+++ src/karzoun_x/simulator/__init__.py
@@ -0,0 +1,15 @@
+from .fault_testbed import (
+    FaultDefinition,
+    FaultScenario,
+    fault_definitions,
+    generate_scenarios,
+    knowledge_documents,
+)
+
+__all__ = [
+    "FaultDefinition",
+    "FaultScenario",
+    "fault_definitions",
+    "generate_scenarios",
+    "knowledge_documents",
+]
